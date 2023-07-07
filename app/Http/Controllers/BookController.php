b5ae6034629080c2855f6da509a14829ce894fb8
engineer_tacky <tsubasa801@gmail.com>
2023-07-07 09:07:53 +0900

    キャッシュを使用するように修正

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,7 +26,9 @@ class BookController extends Controller
           'highest_rated_last_6months' => $books->highestRatedLast6Months(),
           default => $books->latest()
         };
-        $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
@@ -50,9 +52,15 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+      $cacheKey = 'book:' . $book->id;
+
+      $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+          'reviews' => fn($query) => $query->latest()
+      ]));
+
+      return view('books.show', ['book' => $book]);
     }
 
     /**
